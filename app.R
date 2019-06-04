1598e9a27270c6bc52d31c2bfc09a4ccb0bcfe29
ArtPoon <artpoon@gmail.com>
2019-06-03 23:54:29 -0400

    starting #16

---
+++ app.R
@@ -0,0 +1,25 @@
+library(shiny)
+library(clmp)
+
+
+ui <- fluidPage(
+  titlePanel("clmp: clustering with Markov-modulated Poisson processes"),
+  sidebarLayout(
+    sidebarPanel(
+      textAreaInput(
+        inputId="newick", 
+        label="Input tree",
+        value=""
+        )
+    ),
+    mainPanel(
+      plotOutput(outputId="ggtree")
+    )
+  )
+)
+
+server <- function(input, output, session) {
+  
+}
+
+shinyApp(ui, server)
